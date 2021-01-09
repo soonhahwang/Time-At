10cb845ee91b75fd4886a40d35a062d834b8f0fc
soonhahwang <soonha1008@gmail.com>
2021-01-09 10:15:11 +0900

    NY bug fix

--- Timezonebot.py
+++ Timezonebot.py
@@ -12,37 +12,28 @@ TOKEN = os.getenv('bot_token')
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game('with time'))
-    print("Timebot is ready")
 
 @client.event
 async def on_message(message):
 
-
-
-
     if message.author == client.user:
         return
 ##################################################################################################################################################################
-   
     if message.content.startswith('!time'):
         region = message.content[5:]
+        if "NewYork" in region:
+            region = "NY"
         geolocator = Nominatim(user_agent="Discord bot")
         location = geolocator.geocode(region)
-
         local_zone = TimezoneFinder(in_memory=True)
         local_zone = local_zone.timezone_at(lng= location.longitude, lat = location.latitude)
         await message.channel.send("**"+datetime.now(timezone(local_zone)).strftime("%I:%M:%S %p")+"**, "+"Timezone: "+local_zone)
 
-    elif message.content.startswith('!testADD'):
-        await message.channel.send(datetime.now(pytz.timezone('Europe/Paris')))
-
-
+    # if message.content.startswith('!difference'):
+    #     regions = split()
 ##################################################################################################################################################################
 
             
         
 
-client.run('')
+client.run('Nzg1NDkwOTAzNTIyNDc2MDMy.X84naQ.tLHSEGkU3aV3vIQmDzbrDVTziiU')
